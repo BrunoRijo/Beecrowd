8ac21d676a152db4a1436e2c55a6771e3b57c5f5
BrunoRijo <bruno_rijo@hotmail.com>
2022-08-29 13:26:56 -0300

    SQL - Questions

---
+++ SQL/2622.sql
@@ -0,0 +1,7 @@
+/*
+O setor de vendas quer fazer uma promoção para todos os clientes que são pessoas jurídicas. Para isso você deve exibir o nome dos clientes que sejam pessoa jurídica.
+*/
+
+SELECT c.name
+FROM customers c
+INNER JOIN legal_person p ON p.id_customers = c.id
---
+++ SQL/2623.sql
@@ -0,0 +1,14 @@
+/*
+O setor de vendas precisa de um relatório para saber quais produtos estão sobrando em estoque.
+
+Para você ajudar o setor de vendas, exiba o nome do produto e o nome da categoria, para os produtos cuja quantidade seja maior que 100 e o código da categoria seja 1,2,3,6 ou 9. Mostre essas informações em ordem crescente pelo código da categoria.
+*/
+
+SELECT
+    prod.name
+    ,cat.name
+FROM products prod
+INNER JOIN categories cat ON cat.id = prod.id_categories
+WHERE prod.amount > 100 
+    AND cat.id in (1,2,3,6,9)
+ORDER BY cat.id
