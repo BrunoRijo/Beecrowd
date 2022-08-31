e7a0996cd968d731be6ceeb4189cb484c059086f
BrunoRijo <bruno_rijo@hotmail.com>
2022-08-31 19:29:45 -0300

    SQL

--- SQL/2609.sql
+++ SQL/2609.sql
@@ -1,3 +1,9 @@
+/*
+Como de costume o setor de vendas está fazendo uma análise de quantos produtos temos em estoque, e você poderá ajudar eles.
+
+Então seu trabalho será exibir o nome e a quantidade de produtos de cada uma categoria.
+*/
+
 SELECT
     cat.name
     ,SUM(prod.amount) sum
